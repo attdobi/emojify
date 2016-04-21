41ffc6d17e99586b3159c5c7f593e5b12df6b0ed
Attila Dobi <adobi@lbl.gov>
2016-04-21 10:03:38 -0600

    mine LIB

--- Mine/Mongo2SQL_usage_aws.py
+++ Mine/Mongo2SQL_usage_aws.py
@@ -29,7 +29,7 @@ else:
 #core numbers range from 0,1,2
     
 if __name__ == "__main__":
-	for tweet in tweets.find(no_cursor_timeout=True)[5858586:]:
+	for ii,tweet in enumerate(tweets.find(no_cursor_timeout=True)[5858586:]):
 		if (ii%cores==core_number):
 			mine_tweets(tweet,Mongo=True)
 
