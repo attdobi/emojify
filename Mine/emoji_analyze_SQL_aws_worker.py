d6ed1a5e4308125fd3cf9d5c82e0f48171d122f0
Attila Dobi <adobi@lbl.gov>
2016-04-27 20:49:35 -0600

    AWS emoji workers odd/even

--- Mine/emoji_analyze_SQL_aws_worker.py
+++ Mine/emoji_analyze_SQL_aws_worker.py
@@ -31,7 +31,7 @@ else:
 run=True
 if __name__ == "__main__":
 	while run:
-		cur.execute("SELECT tweet_id from has_emoji WHERE MOD(tweet_id,2)=%s order by tweet_id DESC limit 1;",core_number)#find last processed id
+		cur.execute("SELECT tweet_id from has_emoji WHERE MOD(tweet_id,2)=%s order by tweet_id DESC limit 1;",(core_number,))#find last processed id
 		last_id=cur.fetchone()
 		#last_id=15532115
 		cur.execute("SELECT * from tweet_dump WHERE (id>%s AND MOD(id,2)=%s);",(last_id,core_number)) 
