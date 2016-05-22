5192058b02d061589dfa5e1f2771b5ce8c0853ad
Attila Dobi <adobi@lbl.gov>
2016-05-22 08:11:24 -0600

    aws worker

--- Mine/emoji_analyze_SQL_aws_worker.py
+++ Mine/emoji_analyze_SQL_aws_worker.py
@@ -18,8 +18,8 @@ OAUTH_TOKEN_SECRET = 'Jq9oTRMUjHRgA7NkJLLHIEyjtCRhYiFHdWkpBw28IBtHG'
 #set up parallel cores:, we will use 3
 if len(sys.argv) == 2:
 	core_number=int(sys.argv[1])-1
-	cores=4
-	print('running on core {:d} of 4'.format(core_number+1))
+	cores=2
+	print('running on core {:d} of 2'.format(core_number+1))
 else:
 	#run on 1 core
 	print('running on one core')
@@ -33,9 +33,9 @@ if __name__ == "__main__":
 	while run:
 		conn = psycopg2.connect("host=172.31.22.77 port=5432 dbname=emoji_db user=postgres password=darkmatter")
 		cur = conn.cursor()
-		cur.execute("SELECT tweet_id from has_emoji WHERE MOD(tweet_id,4)=%s order by tweet_id DESC limit 1;",(core_number,))#find last processed id
+		cur.execute("SELECT tweet_id from has_emoji WHERE MOD(tweet_id,2)=%s order by tweet_id DESC limit 1;",(core_number,))#find last processed id
 		last_id=cur.fetchone()
-		cur.execute("SELECT * from tweet_dump WHERE (id>%s AND MOD(id,4)=%s) LIMIT 10000;",(last_id,core_number)) 
+		cur.execute("SELECT * from tweet_dump WHERE (id>%s AND MOD(id,2)=%s) LIMIT 10000;",(last_id,core_number)) 
 		#where id>tweet_id, only odd or even
 		SQL_result=cur.fetchall()
 		print(len(SQL_result))
