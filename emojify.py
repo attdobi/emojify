60a467fd03673f41435bc11f9489a3633cc70ac0
attdobi <attdobi@gmail.com>
2016-06-24 09:08:52 -0700

    require authentication

--- emojify.py
+++ emojify.py
@@ -11,6 +11,33 @@ locale.setlocale(locale.LC_ALL, 'en_US')
 Emoji=emoji_lib()
 Tall=TallLabs_lib()
 
+#Setup Authentication #########################################
+from functools import wraps
+from flask import request, Response
+
+def check_auth(username, password):
+	"""This function is called to check if a username /
+	password combination is valid.
+	"""
+	return username == 'insight' and password == 'demo'
+
+def authenticate():
+	"""Sends a 401 response that enables basic auth"""
+	return Response(
+	'Could not verify your access level for that URL.\n'
+	'You have to login with proper credentials', 401,
+	{'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+	@wraps(f)
+	def decorated(*args, **kwargs):
+		auth = request.authorization
+		if not auth or not check_auth(auth.username, auth.password):
+			return authenticate()
+		return f(*args, **kwargs)
+	return decorated
+############# End Authentication ############################
+
 application = Flask(__name__)
 
 @application.route("/")
@@ -52,6 +79,7 @@ def train():
 def slides():
 	return render_template("slides.html")
 @application.route("/demo")
+@requires_auth
 def demo():
 	return render_template("search.html")
 	
