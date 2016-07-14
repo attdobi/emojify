2110fda65963b80186ef540c9b1eb44ab4b44822
attdobi <attdobi@gmail.com>
2016-07-14 12:51:13 -0700

    emoji2vec

--- emojify.py
+++ emojify.py
@@ -179,7 +179,7 @@ def context():
 def get_emojivec():
 	word = request.args.get('word')
 	xdata, ydata=Emoji.emoji2vec_lookup(word=word)
-	ystr=['{0.3f}'.format(val) for val in ydata]#format str here, not in json
+	#ystr=['{0.3f}'.format(val) for val in ydata]#format str here, not in json
 	return jsonify({"values":[{"rank":rank+1,"value":sim,"label":emoji} for rank,(sim,emoji) in enumerate(zip(ydata,xdata))],"key": "Serie 1"})
 	
 @application.route("/db")
