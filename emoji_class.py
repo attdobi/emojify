c20e012928302e5ab94ea73e0eaa175ce4b6ac7c
attdobi <attdobi@gmail.com>
2016-11-16 14:51:45 -0800

    rand range for art and context

--- emoji_class.py
+++ emoji_class.py
@@ -827,8 +827,10 @@ class emoji_lib:
 	def sample_art(self):
 		#Removed TABLESAMPLE SYSTEM (0.5) ... was getting too slow, replaced with last_id-1000000
 		last_id=self.get_last_id()-1000000
+		rand_start=random.randint(last_id)
+		rand_end=rand_start+1000000
 		#self.cur.execute("SELECT b.text from emoji_tweet a join tweet_dump b on a.tweet_id=b.id WHERE (a.emojiCountSum > 30) order by random() limit 100;")
-		self.cur.execute("SELECT b.text from (SELECT * from emoji_tweet WHERE id > {:d}) as a join tweet_dump b on a.tweet_id=b.id WHERE (a.emojiCountSum > 30) limit 100;".format(last_id))
+		self.cur.execute("SELECT b.text from (SELECT * from emoji_tweet WHERE id BETWEEN {:d} AND {:d}) as a join tweet_dump b on a.tweet_id=b.id WHERE (a.emojiCountSum > 30) limit 100;".format(last_id,rand_start,rand_end))
 		art=[_u(text[0]) for text in self.cur.fetchall()]
 		return '\n\n'.join(art)
 		
@@ -847,8 +849,10 @@ class emoji_lib:
 			
 		#Removed TABLESAMPLE SYSTEM (0.5) ... was getting too slow. replaced with last_id-1000000
 		last_id=self.get_last_id()-1000000
+		rand_start=random.randint(last_id)
+		rand_end=rand_start+1000000
 		#self.cur.execute("SELECT text from emoji_tweet WHERE (LOWER(text) LIKE '%{:s}%' {:s} ) order by random() DESC limit 1000;".format(_u(word),lang))
-		self.cur.execute("SELECT text from emoji_tweet WHERE (LOWER(text) LIKE '%{:s}%' {:s} and id > {:d}) limit 1000;".format(_u(word),lang,last_id))
+		self.cur.execute("SELECT text from emoji_tweet WHERE (LOWER(text) LIKE '%{:s}%' {:s} and id BETWEEN {:d} AND {:d}) limit 1000;".format(_u(word),lang,rand_start,rand_end))
 		result=[_u(text[0]) for text in self.cur.fetchall()]
 		return '\n'.join(result)
 	######### emojify code: ################################################
