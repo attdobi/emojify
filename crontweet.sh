fadb2941d4a7fa9564cb3c863cf36b08051c1f0f
attdobi <attdobi@gmail.com>
2017-09-17 07:07:46 -0400

    adding .ignore

--- crontweet.sh
+++ crontweet.sh
@@ -7,8 +7,9 @@ else
         #mailing program
 	#echo "restarting emoji_mine"
         #cpulimit --limit 50 nice python /home/ubuntu/emojify/emoji_mine_aws.py #"emojipy was not running...  Restarted."
-        python /home/ubuntu/emojify/Mine/twitter_mine_SQL.py #restarting twitter mine
-	#tasket -c 0 command #limits process to core number 0
+        python /home/attila/emojify/Mine/twitter_mine_SQL.py #restarting twitter mine
+	#change to /ubuntu on AWS
+#tasket -c 0 command #limits process to core number 0
 	#nice also limits python usage when another intense program opens
 exit 0
 fi
